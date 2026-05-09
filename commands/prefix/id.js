f0b382b34f9c7ba9455641e0ae66ac4004e7482b
mistvle <mistle.roblox@gmail.com>
2026-05-09 11:36:50 -0700

    A

---
+++ commands/prefix/id.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "id",
+    
+    async execute (message) {
+        await message.reply({
+  "flags": 32768,
+  "components": [
+    {
+      "type": 17,
+      "components": [
+        {
+          "type": 10,
+          "content": `Your **Discord ID** is ${message.author.id}.`
+        }
+      ]
+    }
+  ]
+})
+    }
+}
