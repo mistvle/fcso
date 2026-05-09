beb96f7c49a1dcfe3f6c5be199fde83c1760389f
mistvle <mistle.roblox@gmail.com>
2026-05-09 11:28:49 -0700

    A

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,13 +2,27 @@ module.exports = {
   name: "messageCreate",
 
   async execute(client, message) {
+
+    console.log("MESSAGE EVENT FIRED");
+    console.log(message.content);
+
     if (message.author.bot) return;
     if (!message.content.startsWith(client.prefix)) return;
 
     const args = message.content.slice(client.prefix.length).trim().split(/ +/);
+
+    console.log(args);
+
     const cmdName = args.shift().toLowerCase();
 
+    console.log("COMMAND:", cmdName);
+
+    console.log(client.prefixCommands);
+
     const cmd = client.prefixCommands.get(cmdName);
+
+    console.log("FOUND CMD:", cmd);
+
     if (!cmd) return;
 
     try {
